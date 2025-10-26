00f12c1d8947a672e4dae70a8ecf0af5e9913f57
anno232 <jiangjjj315@gmail.com>
2025-10-26 21:34:16 +0800

    hw1 hw2 hw3提交

---
+++ HW1/ls3 hw1/MDK-ARM/RTE/_ls3_hw1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ls3 hw1' 
+ * Target:  'ls3 hw1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
